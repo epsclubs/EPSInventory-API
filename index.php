752d0ba965a86f60adec34d8326e79304a7de7f3
Ben Zhang <benzhangniu@gmail.com>
2014-12-30 20:00:49 -0800

    Initial Commit - test4

--- index.php
+++ index.php
@@ -1,7 +1,28 @@
 <?php
 
-$mongo = new MongoClient();
-var_dump($mongo);
+$m = new MongoClient('mongodb://test:test@dogen.mongohq.com:10080/epsclubs');
+// echo '<pre>';
 // phpinfo();
 
-echo 'test 3';
+// echo 'test 3';
+
+$db = $m->epsclubs;
+
+// select a collection (analogous to a relational database's table)
+$collection = $db->cartoons;
+
+// add a record
+$document = array( "title" => "Calvin and Hobbes", "author" => "Bill Watterson" );
+$collection->insert($document);
+
+// add another record, with a different "shape"
+$document = array( "title" => "XKCD", "online" => true );
+$collection->insert($document);
+
+// find everything in the collection
+$cursor = $collection->find();
+
+// iterate through the results
+foreach ($cursor as $document) {
+  echo $document["title"] . "\n";
+}
---
+++ models/Object.php
@@ -0,0 +1,6 @@
+<?php
+
+public $type; // int or string
+public $name; // string
+public $properties; // Array of strings
+public $Lists; // Array of arrays
